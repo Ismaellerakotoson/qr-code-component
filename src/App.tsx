3e39b1765260fbf15b3d50ffbd804aeb416e7732
Ismaellerakotoson <ismaellefiononantsoa@gmail.com>
2026-05-31 22:21:06 +0300

    code review

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import { QRCodeSVG } from "qrcode.react";
 
 function App() {
+  const txtStyles = "text-color max-w-xs";
   return (
     <div className="flex background-color min-h-screen text-black items-center justify-center">
-      <div className="blackground-white p-4 rounded-xl max-width min-width">
+      <div className="bg-white p-4 rounded-xl max-min-width">
         <div className="bg-blue-500 flex items-center justify-center p-10 rounded-xl">
           <div className="p-8">
             <QRCodeSVG value="https://frontendmentor.io" 
@@ -14,10 +15,10 @@ function App() {
         </div>
         <div className="text-center mt-5">
           <div className="font-bold mb-3">
-            <h1 className="title-color max-w-xs">Improve your front-end skills by building projects</h1>
+            <h1 className={txtStyles}>Improve your front-end skills by building projects</h1>
           </div>
           <div className="text-sm font-normal">
-            <p className="text-color max-w-xs">
+            <p className={txtStyles}>
               Scan the QR-code to visit Frontend Mentor and take your coding
               skills to the next level
             </p>
